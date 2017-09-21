5e304e336fc54a3ece745fe5695548f8f228ff39
Joseph Medley <jmedley@google.com>
2017-09-22 08:35:44 +0900

    Write missing pages to file.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const fs = require('fs');
 const https = require('https');
+const path = require('path');
 const urllist = require('./urllist');
 
 const inputList = 'in/confluence-test.csv';
@@ -10,14 +12,34 @@ const options = {
   path: ''
 }
 
-function test(url) {
+let handle;
+fs.open('results.csv', 'w', (e, fd) => {
+  if (e) {
+    if (e.code === 'EEXIST') {
+      console.error("[WALKER] File already exists");
+      return;
+    } else {
+      throw e;
+    }
+  } else {
+    handle = fd;
+  }
+})
+
+function testUrl(url) {
   options.path = url;
   const req = https.get(options);
   req.end();
   req.once('response', res => {
-    console.log(res.statusCode);
+    // console.log(res.statusCode);
+    if (res.statusCode >= 300) {
+      var link = path.join(options.protocol, options.host, url) + "\n";
+      fs.write(handle, link, () => {
+        // console.log(arguments[0]);
+      });
+    }
   })
 }
 
 const list = new urllist.URLList(inputList);
-list.get(test);
+list.get(testUrl);
