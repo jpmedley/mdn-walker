c91b20cc96ff0c4973166c4ac1692ceafb902916
Joseph Medley <jmedley@google.com>
2017-09-28 14:09:24 -0700

    More make URLList a generator.

--- test_urllist.js
+++ test_urllist.js
@@ -1,26 +1,40 @@
 'use strict';
 
+const assert = require('assert');
 const urllist = require('./urllist');
 
 const TEST_FILE = 'in/confluence-test.csv';
-const list = new urllist.URLList(TEST_FILE);
 
-function _ReadURLList(excludeMatching = false) {
-  list.get(line => {
-    console.log(line);
-  }, excludeMatching)
+function _TestSetup(options) {
+  return new urllist.URLList(TEST_FILE, options.excludeMatching);
 }
 
 function ListAll() {
-  console.log('\n[WALKER TEST] Starting URLList with ' + TEST_FILE + '.');
-  _ReadURLList();
+  let next;
+  const options = {excludeMatching: false};
+  const list = _TestSetup(options);
+  do {
+    next = list.next();
+    // assert.assert(next.value, 'It worked.');
+  } while (next.done == false);
 }
 
-function ExcludeMatching() {
-  console.log('\n[WALKER TEST] Starting URLList with ' + TEST_FILE +
-              ', excluding unchanged APIs.');
-  _ReadURLList(true);
-}
+// function _ReadURLList(excludeMatching = false) {
+//   list.get(line => {
+//     console.log(line);
+//   }, excludeMatching)
+// }
+//
+// function ListAll() {
+//   console.log('\n[WALKER TEST] Starting URLList with ' + TEST_FILE + '.');
+//   _ReadURLList();
+// }
+//
+// function ExcludeMatching() {
+//   console.log('\n[WALKER TEST] Starting URLList with ' + TEST_FILE +
+//               ', excluding unchanged APIs.');
+//   _ReadURLList(true);
+// }
 
-ExcludeMatching();
+// ExcludeMatching();
 ListAll();
--- urllist.js
+++ urllist.js
@@ -4,26 +4,34 @@ const path = require('path');
 const sl = require('./sourcelist');
 const URL_BASE = '/en-US/docs/Web/API';
 
-function *URLList(source, diffs_only=false) {
-  let source = source;
-  let interface;
+function URLList(source, diffs_only=false) {
+  let interfaceName;
   this.list = new Array();
   let reader = new sl.ConfluenceSourceList(source);
   reader.get(line => {
     let url;
     let props = line.split(',');
-    if (props[0]!=interface) {
-      interface = props[0];
+    if (props[0]!=interfaceName) {
+      interfaceName = props[0];
       url = path.join(URL_BASE, props[0]);
       this.list.push(url);
     }
     url = path.joing(URL_BASE, props[0], props[1]);
     this.list.push(url);
   }, diffs_only);
+}
+
+URLList.prototype._get = function*() {
   while (this.list.length > 0) {
     let ret = this.list.shift();
     yield ret;
   }
+}
+
+URLList.prototype.next = function() {
+  // Double-layered so that client code does not need to do what
+  //  I've done below.
+  return this._get().next();
 }
 
 // URLList.prototype.get_ = function(callback, diffs_only) {
@@ -31,8 +39,8 @@ function *URLList(source, diffs_only=false) {
 //   this.list.get(line => {
 //     let url;
 //     const props = line.split(',');
-//     if (props[0]!=this.interface){
-//       this.interface = props[0];
+//     if (props[0]!=this.interfaceName){
+//       this.interfaceName = props[0];
 //       url = path.join(URL_BASE, props[0]);
 //       callback(url);
 //     }
